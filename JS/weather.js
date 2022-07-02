800d053cb9dae12ca2a6754747601b3ec757211c
raed bahri <raedbahri90@gmail.com>
2022-07-02 10:59:01 +0100

    completed

--- JS/weather.js
+++ JS/weather.js
@@ -1,97 +1,101 @@
-const locationElement = document.getElementById("location");
-const mainSectionIconElement = document.getElementById("main-section-icon");
-const temperatureElement = document.getElementById("temperature");
-const zoneElement = document.getElementById("zone");
-const descriptionElement = document.getElementById("description");
+const API_KEY = "ea8ca11a65b712b5c0aea20069d39890";
+
+const sunsetElement = document.getElementById("sunset-time");
+const sunriseElement = document.getElementById("sunrise-time");
 const windElement = document.getElementById("wind");
-const sunriseTimeElement = document.getElementById("sunrise-time");
-const sunsetTimeElement = document.getElementById("sunset-time");
-const firstDayIconElement = document.getElementById("firstDay-icon");
-const firstDayWeatherElement = document.getElementById("firstDay-weather");
-const firstDayMaxTempElement = document.getElementById("firstDayMax-temperature");
-const firstDayMinTempElement = document.getElementById("firstDayMin-temperature");
-const secondDayIconElement = document.getElementById("secondDay-icon");
-const secondDayWeatherElement = document.getElementById("secondDay-weather");
-const secondDayMaxTempElement = document.getElementById("secondDayMax-temperature");
-const secondDayMinTempElement = document.getElementById("secondDayMin-temperature");
-const thirdDayIconElement = document.getElementById("thirdDay-icon");
-const thirdDayWeatherElement = document.getElementById("thirdDay-weather");
-const thirdDayMaxTempElement = document.getElementById("thirdDayMax-temperature");
-const thirdDayMinTempElement = document.getElementById("thirdDayMin-temperature");
-const forthDayIconElement = document.getElementById("forthDay-icon");
-const forthDayWeatherElement = document.getElementById("forthDay-weather");
-const forthDayMaxTempElement = document.getElementById("forthDayMax-temperature");
-const forthDayMinTempElement = document.getElementById("forthDayMin-temperature");
-const fifthDayIconElement = document.getElementById("fifthDay-icon");
-const fifthDayWeatherElement = document.getElementById("fifthDay-weather");
-const fifthDayMaxTempElement = document.getElementById("fifthDayMax-temperature");
-const fifthDayMinTempElement = document.getElementById("fifthDayMin-temperature");
-const API_KEY = "3a61d03d191e00e1fa908edb1ff5d13c";
+const tempElement = document.getElementById("temperature");
+const descriptionElement = document.getElementById("description");
+const mainIconElement = document.getElementById("main-section-icon");
+const zoneElement = document.getElementById("zone");
+const dailyForcastElement = document.getElementById("days-forecast");
+const daysElementArray = [];
+const days = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+];
+let data = null;
+let geoData = null;
+async function getWeatherData() {
+	navigator.geolocation.getCurrentPosition((success) => {
+		getData = success.coords;
+	});
+	for (let index = 0; index < daysElementArray.length; index++) {
+		const element = daysElementArray[index];
+		element.remove();
+	}
+	let res = {};
+	if (geoData) {
+		res = await fetch(
+			`https://api.openweathermap.org/data/2.5/onecall?lat=${getData.latitude}&lon=${getData.longitude}&units=metric&exclude=minutely,hourly,alerts&appid=${API_KEY}`
+		);
+	} else {
+		res = await fetch(
+			`https://api.openweathermap.org/data/2.5/onecall?lat=35.8240876&lon=10.6247323&units=metric&exclude=minutely,hourly,alerts&appid=${API_KEY}`
+		);
+	}
 
-  function getWeatherData() {
-    navigator.geolocation.getCurrentPosition((success) => {
-    let { latitude, longitude } = success.coords;
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data =>{
-    console.log(data)
-    showWeatherData(data);
-    })
-  })
+	data = await res.json();
+	const sunrise = new Date(data?.current?.sunrise).toLocaleTimeString("en");
+	sunriseElement.innerText = `${sunrise}`;
+	const sunset = new Date(data?.current?.sunset).toLocaleTimeString("en");
+	sunsetElement.innerText = `${sunset}`;
+	windElement.innerText = `${data.current.wind_speed} km/h`;
+	tempElement.innerText = `${data.current.temp} °C`;
+	descriptionElement.innerText = `${data.current.weather[0].main}, ${data.current.weather[0].description}`;
+	mainIconElement.src = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`;
+	zoneElement.innerText = data.timezone;
+	for (let index = 0; index < 5; index++) {
+		const element = data.daily[index];
+		const dayCard = document.createElement("div");
+		daysElementArray.push(dayCard);
+		dayCard.className = "day-card";
+		const imgWeather = document.createElement("img");
+		imgWeather.src = `http://openweathermap.org/img/wn/${element.weather[0].icon}.png`;
+		imgWeather.className = "bts-icon";
+		dayCard.appendChild(imgWeather);
+		const h2element = document.createElement("h2");
+		h2element.className = "day";
+		h2element.innerText = days[new Date(element.dt * 1000).getDay()];
+		dayCard.appendChild(h2element);
+		const minmax = document.createElement("div");
+		minmax.className = "min-max";
+		const maxElement = document.createElement("max");
+		maxElement.className = "max";
+		const maxIcon = document.createElement("div");
+		maxIcon.className = "min-max-icon";
+		const maxfontIcon = document.createElement("i");
+		maxfontIcon.className = "fa-solid fa-sort-up";
+		maxIcon.appendChild(maxfontIcon);
+		const maxP = document.createElement("p");
+		maxP.innerText = `${Math.floor(element.temp.max)}°C`;
+		maxElement.appendChild(maxIcon);
+		maxElement.appendChild(maxP);
+
+		const minElement = document.createElement("min");
+		minElement.className = "min";
+		const minIcon = document.createElement("div");
+		minIcon.className = "min-max-icon";
+		const minfontIcon = document.createElement("i");
+		minfontIcon.className = "fa-solid fa-sort-down";
+		minIcon.appendChild(minfontIcon);
+		const minP = document.createElement("p");
+		minP.innerText = `${Math.floor(element.temp.min)}°C`;
+
+		minElement.appendChild(minIcon);
+		minElement.appendChild(minP);
+
+		minmax.appendChild(maxElement);
+		minmax.appendChild(minElement);
+		dayCard.appendChild(minmax);
+		dailyForcastElement.appendChild(dayCard);
+	}
 }
 
-
-function showWeatherData(data){
-  let { sunrise, sunset, wind_speed, temp } = data.current;
-  let weatherDesc = data.current.weather[0].description;
-  let timezone = data.timezone;
-  temperatureElement.innerText=`${temp}°C`;
-  zoneElement.innerText=timezone;
-  descriptionElement.innerText=weatherDesc;
-  windElement.innerText=`${wind_speed} km/h`;
-  sunriseTimeElement.innerText=convertTime(sunrise);
-  console.log(typeof sunrise);
-  sunsetTimeElement.innerText=convertTime(sunset);
-  console.log(typeof sunrise);
-  firstDayWeatherElement.innerText=data.daily[1].weather[0].main;
-  firstDayMaxTempElement.innerText=`${data.daily[1].temp.max}°`;
-  firstDayMinTempElement.innerText=`${data.daily[1].temp.min}°`;
-  secondDayWeatherElement.innerText=data.daily[2].weather[0].main;
-  secondDayMaxTempElement.innerText=`${data.daily[2].temp.max}°`;
-  secondDayMinTempElement.innerText=`${data.daily[2].temp.min}°`;
-  thirdDayWeatherElement.innerText=data.daily[3].weather[0].main;
-  thirdDayMaxTempElement.innerText=`${data.daily[3].temp.max}°`;
-  thirdDayMinTempElement.innerText=`${data.daily[3].temp.min}°`;
-  forthDayWeatherElement.innerText=data.daily[4].weather[0].main;
-  forthDayMaxTempElement.innerText=`${data.daily[4].temp.max}°`;
-  forthDayMinTempElement.innerText=`${data.daily[4].temp.min}°`;
-  fifthDayWeatherElement.innerText=data.daily[5].weather[0].main;
-  fifthDayMaxTempElement.innerText=`${data.daily[5].temp.max}°`;
-  fifthDayMinTempElement.innerText=`${data.daily[5].temp.min}°`;
-  var currentWeatherIcon=data.current.weather[0].icon;
-  var firstDayIcon=data.daily[1].weather[0].icon;
-  var secondDayIcon=data.daily[2].weather[0].icon;
-  var thirdDayIcon=data.daily[3].weather[0].icon;
-  var forthDayIcon=data.daily[4].weather[0].icon;
-  var fifthDayIcon=data.daily[5].weather[0].icon;
-  var iconurl = "http://openweathermap.org/img/w/" + currentWeatherIcon + ".png";
-  console.log(iconurl);
-  mainSectionIconElement.src=iconurl;
-  firstDayIconElement.src="http://openweathermap.org/img/w/" + firstDayIcon + ".png";
-  secondDayIconElement.src="http://openweathermap.org/img/w/" + secondDayIcon + ".png";
-  thirdDayIconElement.src="http://openweathermap.org/img/w/" + thirdDayIcon + ".png";
-  forthDayIconElement.src="http://openweathermap.org/img/w/" + forthDayIcon + ".png";
-  fifthDayIconElement.src="http://openweathermap.org/img/w/" + fifthDayIcon + ".png";
-  function convertTime(x){
-    var hours =new Date(x*1000).getHours();
-    if (hours>12)
-    {
-      hours=hours-12;
-    }
-    var minutes =new Date(x*1000).getMinutes();
-    var convertedTime=`${hours}:${minutes}`;
-    return convertedTime;
-  }
-
-
-}
-
-
+document.addEventListener("load", () => {
+	getWeatherData();
+});
